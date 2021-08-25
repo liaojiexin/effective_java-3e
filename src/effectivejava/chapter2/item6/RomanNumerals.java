8d2812cb2d9049d938d649f501a0e71f95ea3fbb
liaojiexin <570839281@qq.com>
2021-08-26 00:06:49 +0800

    c2.i7

--- src/effectivejava/chapter2/item6/RomanNumerals.java
+++ src/effectivejava/chapter2/item6/RomanNumerals.java
@@ -4,12 +4,17 @@ import java.util.regex.Pattern;
 // Reusing expensive object for improved performance (Pages 22 and 23)
 public class RomanNumerals {
     // Performance can be greatly improved! (Page 22)
+    //反面教材String.matches方法是最易于查看一个字符串是否与正则表达式相匹配。
+    // 但是并不合适在注重性能的情形中重复使用。因为他在内部为正则表达式创建了一个
+    //Pattern实例，却使用了一次就可以被垃圾回收了
     static boolean isRomanNumeralSlow(String s) {
         return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
                 + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
     }
 
     // Reusing expensive object for improved performance (Page 23)
+    //显示地将正则表达式编译成一个Pattern实例，让它成为类初始化的一部分，并将它缓存起来
+    //每次调用isRomanNumeralFast方法的时候就重用同个实例
     private static final Pattern ROMAN = Pattern.compile(
             "^(?=.)M*(C[MD]|D?C{0,3})"
                     + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
--- src/effectivejava/chapter2/item6/Sum.java
+++ src/effectivejava/chapter2/item6/Sum.java
@@ -3,6 +3,9 @@ package effectivejava.chapter2.item6;
 import java.util.Comparator;
 
 // Hideously slow program! Can you spot the object creation? (Page 24)
+//这段程序结果是对的，但是运行性能比实际情况慢很多，因为打错了一个字符(变量被声明为Long而不是long)
+//意味着程序构造了大约2^31个多余的Long实例(大约每次往Long sum中增加long时构造一个实例)
+//所以要优先使用基本类型而不是装箱基本类型，要当心无意识的自动装箱
 public class Sum {
     private static long sum() {
         Long sum = 0L;
