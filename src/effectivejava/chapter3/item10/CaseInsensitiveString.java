ef4da864dba0dda0cee99c568c4701f70aeac9d6
liaojiexin <570839281@qq.com>
2021-08-30 22:36:15 +0800

    c3.item10

--- src/effectivejava/chapter3/item10/CaseInsensitiveString.java
+++ src/effectivejava/chapter3/item10/CaseInsensitiveString.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+//违反对称性
 // Broken - violates symmetry!  (Page 39)
 public final class CaseInsensitiveString {
     private final String s;
@@ -13,6 +14,8 @@ public final class CaseInsensitiveString {
     }
 
     // Broken - violates symmetry!
+    //equalsIgnoreCase不区分大小写对比两个字符串
+    //这个equals方法它企图与普通的字符串对象进行互操作。
     @Override public boolean equals(Object o) {
         if (o instanceof CaseInsensitiveString)
             return s.equalsIgnoreCase(
@@ -24,6 +27,7 @@ public final class CaseInsensitiveString {
 
     // Demonstration of the problem (Page 40)
     public static void main(String[] args) {
+        //cis.equals(s)为true，s.equals(cis)为false，因为s为String类型的equals方法不知道不区分大小写的字符串
         CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
         String s = "polish";
 
@@ -34,6 +38,7 @@ public final class CaseInsensitiveString {
     }
 
 //    // Fixed equals method (Page 40)
+    //正确写法，把企图与String互操作的代码去掉
 //    @Override public boolean equals(Object o) {
 //        return o instanceof CaseInsensitiveString &&
 //                ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
